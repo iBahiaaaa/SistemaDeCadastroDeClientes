50248739adab973e6093970db92f9024de4e3cfd
iBahiaaaa <vineira1998@gmail.com>
2026-06-13 00:51:41 -0300

    ajustes banco de dados

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -6,8 +6,8 @@ from supabase import create_client, Client
 load_dotenv()
 
 # Configuração do Supabase
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+SUPABASE_URL = os.environ.get("SUPABASE_URL=https://uwjcimdpdnlriquuzreo.supabase.co")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY=sb_publishable_QLzB0eIRDWULsDzvPooO_A_We6GqI_h")
 
 def get_supabase() -> Client:
     """
